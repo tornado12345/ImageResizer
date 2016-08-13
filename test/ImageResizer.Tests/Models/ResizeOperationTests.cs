c2ef41002d30ae0e4481bc62de883efb83555591
Brice Lambson <bricelam@outlook.com>
2016-08-12 20:37:36 -0700

    Test it

---
+++ test/ImageResizer.Tests/Models/ResizeOperationTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using ImageResizer.Properties;
+using Xunit;
+
+namespace ImageResizer.Models
+{
+    public class ResizeOperationTests
+    {
+        [Theory]
+        [InlineData("Test.jpg")]
+        [InlineData("Test.png")]
+        [InlineData("Test1.gif")]
+        [InlineData("Test2.gif")]
+        [InlineData("Test.tif")]
+        public void Execute_works(string file)
+        {
+            using (var dir = new TestDirectory())
+            {
+                var operation = new ResizeOperation(
+                    file,
+                    dir.Path,
+                    new Settings
+                    {
+                        Sizes = new ObservableCollection<ResizeSize>
+                        {
+                            new ResizeSize
+                            {
+                                Name = "Small",
+                                Fit = ResizeFit.Fit,
+                                Width = 854,
+                                Height = 480,
+                                Unit = ResizeUnit.Pixel
+                            }
+                        },
+                        IgnoreOrientation = true,
+                        FileName = "%1 (%2)"
+                    });
+
+                operation.Execute();
+
+                // TODO: Assert
+            }
+        }
+    }
+}
---
+++ test/ImageResizer.Tests/TestDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ImageResizer
+{
+    public class TestDirectory : IDisposable
+    {
+        public TestDirectory()
+        {
+            Path = System.IO.Path.GetRandomFileName();
+            Directory.CreateDirectory(Path);
+        }
+
+        public string Path { get; }
+
+        public void Dispose()
+            => Directory.Delete(Path, recursive: true);
+    }
+}
